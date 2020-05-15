84cf7d37e0fbe0bbcb1eacfde97fd1bb8dafc470
MiguelOntiveros <MiguelOntiveros@users.noreply.github.com>
2020-05-14 19:07:56 -0500

    se generó lista de actividades

---
+++ src/app/list/agenda.ts
@@ -0,0 +1,13 @@
+import { Dia } from './dia';
+
+export const dias: Dia[] = [
+
+    {
+        dia: new Date('2020-03-10'),
+        hora: new Date('18:00'),
+        actividad: "aplicación de tinte",
+        empleado: "lily",
+    },
+    
+
+]
---
+++ src/app/list/dia.ts
@@ -0,0 +1,6 @@
+export interface Dia {
+    dia: Date;
+    hora: Date;
+    actividad: String;
+    empleado: String;
+}
